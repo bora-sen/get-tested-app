f0905a1b7edaa46a2b7c09dde18e60eaafa76a08
bora-sen <bahattinborasen@gmail.com>
2023-03-22 00:37:09 +0300

    +fix: solve logic

--- src/Pages/SolveTest/index.jsx
+++ src/Pages/SolveTest/index.jsx
@@ -1,13 +1,50 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useContext, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { SolveContext } from "../../Contexts/SolveContext";
+import { getTestWithId } from "../../Firebase/database";
+import Layout from "../../Layout";
+import QuestionComponent from "./Components/QuestionComponent";
+import style from './index.module.css'
 
 function SolveTest() {
-  const {testId} = useParams();
-  console.log(testId);
-  //TODO: Add Solve Test Page
+  const { testId } = useParams();
+  const { Test, setTest, trueAnswerCount } = useContext(SolveContext);
+
+  useEffect(() => {
+    async function setCurrentTest() {
+      let sel_test = await getTestWithId(testId);
+      setTest(sel_test);
+    }
+    setCurrentTest();
+  }, []);
+
+  if (!Test) return <div>Loading..</div>;
+
+  function handleFinishButton(e) {
+    e.preventDefault();
+    console.log("Finished The Test! true count -> ", trueAnswerCount);
+    if (trueAnswerCount > Test.questions.length / 2) {
+      console.log("YOU PASSED !!!!");
+    } else {
+      console.log("YOU DIDN'T PASS");
+    }
+  }
+
   return (
-    <div>SolveTest</div>
-  )
+    <Layout>
+      <section className={style.inner}>
+        <div className={style.title}>
+          <span>{Test.title}</span>
+        </div>
+        <div className={style.question_container}>
+          {Test.questions.map((question, index) => {
+            return <QuestionComponent key={index} questionObj={question} />;
+          })}
+        </div>
+        <button onClick={(e) => handleFinishButton(e)}>Finish The Test!</button>
+      </section>
+    </Layout>
+  );
 }
 
-export default SolveTest
+export default SolveTest;
