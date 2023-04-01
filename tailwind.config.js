8f02eaa2d40e935b3cd6161011cde8317bb3ec28
bora-sen <bahattinborasen@gmail.com>
2023-04-02 02:12:51 +0300

    +refactor: tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,16 +3,16 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,html}"],
   theme: {
     extend: {
-      colors:{
-        "primary-white":"#fcfbfe",
-        "primary-blue":"#002e5d",
-        "primary-blue-pale":"#103360",
-        "primary-red":"#dd253a",
-        "primary-red-pale":"#e9354c"
+      colors: {
+        "primary-white": "#fcfbfe",
+        "primary-blue": "#002e5d",
+        "primary-blue-pale": "#103360",
+        "primary-red": "#dd253a",
+        "primary-red-pale": "#e9354c",
       },
-      fontFamily:{
-        "display":['Bakbak One', 'cursive']
-      }
+      fontFamily: {
+        display: ["Bakbak One", "cursive"],
+      },
     },
   },
   plugins: [],
